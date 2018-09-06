d5f1a0d2c2cff79d8870f20b4f8c751a95420f29
Jonathan Vermeulen <jvermeulen@MacBook-Pro-van-Jonathan.local>
2018-09-06 20:33:16 +0200

    Add proper HTML output to IMS

--- src/main/java/com/riverbed/ims/ImsApplication.java
+++ src/main/java/com/riverbed/ims/ImsApplication.java
@@ -3,8 +3,24 @@ package com.riverbed.ims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
 @SpringBootApplication
 public class ImsApplication {
+	@Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+				registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImsApplication.class, args);
--- src/main/java/com/riverbed/ims/web/ImsController.java
+++ src/main/java/com/riverbed/ims/web/ImsController.java
@@ -9,11 +9,12 @@ import com.riverbed.ims.model.Method;
 import com.riverbed.ims.model.MethodResult;
 import com.riverbed.ims.model.CallResult;;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.client.RestTemplate;
@@ -24,23 +25,62 @@ import org.apache.commons.logging.LogFactory;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 
-@RestController
+@Controller
 public class ImsController {
 
 	private static Log logger = LogFactory.getLog(ImsController.class);
 
 	private RestTemplate restTemplate = new RestTemplate();
 
-	@GetMapping("/**")
-	@ResponseBody
+	@RequestMapping(value = "/**", method = RequestMethod.GET)
 	public String generateLoad() {
-		String output = "Welcome to IMS (the Infinite Monkey Service)!"
-			+ "A Java based transaction/load generator. "
-			+ "Please use a POST request with a transaction JSON as body.";
-		return output;
+		return "intro";
 	}
 
-	@PostMapping("/**")
+	@RequestMapping(value = "/**", method = RequestMethod.POST, headers = "html=yes" ,consumes = {
+        "application/JSON",
+        "application/XML"
+    })
+	public String processTierHtml(@RequestBody Tier thisTier, final HttpServletRequest request, Model model) throws Exception {
+		logger.debug(String.format("Got request for: %s\n%s", request.getRequestURI(), thisTier));
+
+		CallResult callResult = new CallResult();
+
+		for (Method method : thisTier.getMethod()) {
+			Integer mode = method.getMode();
+			Integer min = method.getMin();
+			Integer max = method.getMax();
+			String message = method.getMessage();
+			MethodA thisMethodA = new ByteBuddy()
+  			.subclass(MethodA.class)
+				.name("com.riverbed.ims." + method.getName())
+  			.make()
+				.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+				.getLoaded()
+				.getDeclaredConstructor()
+				.newInstance();
+
+			logger.debug(String.format("Calling method... Name:%s Mode:%d Min:%d Max:%d", thisMethodA.getClass().getName(), mode, min, max));
+			callResult.addResult(thisMethodA.process(mode, min, max, message));
+		}
+
+		if (thisTier.getCall() != null){
+			for (LinkedHashMap call : thisTier.getCall()){
+				logger.debug(String.format("Making request to: %s with body:\n%s", call.get("url"), call));
+				callResult.setCall(restTemplate.postForObject(call.get("url").toString(), call, Object.class));
+				logger.debug(String.format("Result: %s", callResult.getCall()));
+			}
+		} else{
+			logger.debug("No external calls to make...");
+		}
+		model.addAttribute("results", callResult);
+		return "result";
+	}
+
+	@RequestMapping(value = "/**", method = RequestMethod.POST, consumes = {
+        "application/JSON",
+        "application/XML"
+    })
 	@ResponseBody
 	public CallResult processTier(@RequestBody Tier thisTier, final HttpServletRequest request) throws Exception {
 		logger.debug(String.format("Got request for: %s\n%s", request.getRequestURI(), thisTier));
@@ -74,7 +114,7 @@ public class ImsController {
 		} else{
 			logger.debug("No external calls to make...");
 		}
-
 		return callResult;
 	}
+
 }
